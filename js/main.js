0a25d74b18ee0a7a3a42101c4285a77e8a953ac7
ritikdeswal <47329582+ritikdeswal@users.noreply.github.com>
2020-10-01 21:03:01 +0530

    taunt animation

--- js/main.js
+++ js/main.js
@@ -18,3 +18,34 @@ inputs.forEach(input => {
     input.addEventListener('focus', focusFunc);
     input.addEventListener('blur', blurFunc);
 })
+
+// code for taunt animation
+
+const loginButton = document.querySelector('.btn');
+const form = document.querySelector('form');
+
+loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const fields = [...document.querySelectorAll('input')];
+
+    fields.forEach(field => {
+        if(field.value === '') {
+            form.classList.add('validate-error');
+        }
+    });
+
+    const formError = document.querySelector('.validate-error');
+
+    if(formError) {
+        formError.addEventListener('animationend', (event) => {
+            if (event.animationName === 'taunt') {
+                formError.classList.remove('validate-error');
+            }
+        })
+    } 
+
+    else {
+        form.classList.add("form-hide");
+    }
+});
